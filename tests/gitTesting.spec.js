fd944af265c805db46016f1aaf1532aad933e160
butraumamaheshgowd <butraumamahesh123@gmail.com>
2025-07-10 10:46:18 +0530

    CI/CD Pipeline commit2

--- tests/gitTesting.spec.js
+++ tests/gitTesting.spec.js
@@ -1,6 +1,6 @@
 const {test,expect}=require("@playwright/test");
 
-test.only("my testing test", async function({page}){
+test("my testing test", async function({page}){
 
     //await page.goto('https://freelance-learn-automation.vercel.app/signup');
     //await page.getByLabel('Email').fill("umamahesh.gowd@gmail.com");
